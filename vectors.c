2d63e62ba049f7614d6072e9802309d3daaf27a3
Shane-Kyle Chua <90674209+Kairu1999@users.noreply.github.com>
2021-12-27 00:37:52 +0800

    Update vectors.c
    Added 2D Projection

--- vectors.c
+++ vectors.c
@@ -1,4 +1,5 @@
-/*Author: Shane-Kyle Chua*/
+/*Author: Shane-Kyle Chua / Kairu1999*/
+/*This is purely for interest to see how linear algebra's basics can tie in to coding*/
 #include <stdio.h>
 
 struct TwoDimVector {
@@ -10,6 +11,17 @@ struct ThreeDimVector{
     int num1;
     int num2;
     int num3;
+};
+
+struct TwoDimMatrix{
+    struct TwoDimVector e1;
+    struct TwoDimVector e2;
+};
+
+struct ThreeDimMatrix{
+    struct ThreeDimVector e1;
+    struct ThreeDimVector e2;
+    struct ThreeDimVector e3;
 };
 
 /*Calculate the 2 dimensional dot product*/
@@ -24,8 +36,25 @@ int ThreeDimDotProduct(struct ThreeDimVector vector1, struct ThreeDimVector vect
     return dotproduct;
 }
 
+/*Calculate 2D Projection(Non-Matrix)*/
+struct TwoDimVector projection(struct TwoDimVector a1, struct TwoDimVector a2){
+    //if a1 is u, a2 is v, proju(v) = (a2.a1 / a1.a1)a1
+    
+    //declare a new vector to store the projection
+    struct TwoDimVector NewVector;
+    
+    //calculate the projection
+    int dotproductone = TwoDimDotProduct(a1,a2);
+    int dotproducttwo = TwoDimDotProduct(a1,a1);
+    int projection = (dotproductone / dotproducttwo);
+    
+    NewVector.num1 = a1.num1 * projection;
+    NewVector.num2 = a1.num2 * projection;
+    
+    return NewVector;
+}
 
-/*Calculate the three dimensional cross product*/
+/*Calculate the 3D cross product*/
 struct ThreeDimVector crossProduct(struct ThreeDimVector vector1, struct ThreeDimVector vector2){
     struct ThreeDimVector vector3;
     int newval1 = (vector1.num2 * vector2.num3) - (vector1.num3 * vector2.num2);
@@ -38,8 +67,6 @@ struct ThreeDimVector crossProduct(struct ThreeDimVector vector1, struct ThreeDi
     
     return vector3;
 }
-
-
 int main()
 {
     int x1 = 1, y1 = 2, x2 = 3,y2 = 4, z1 = 5, z2 = 6;
@@ -58,9 +85,11 @@ int main()
     int dotproduct = TwoDimDotProduct(v1,v2);
     int threedotproduct = ThreeDimDotProduct(v3,v4);
     struct ThreeDimVector threecrossproduct = crossProduct(v3,v4);
-    printf("%d\n",dotproduct);
-    printf("%d\n",threedotproduct);
-    printf("%d %d %d",threecrossproduct.num1, threecrossproduct.num2, threecrossproduct.num3);
+    struct TwoDimVector twoDProjection = projection(v1,v2);
+    //printf("%d\n",dotproduct);
+    //printf("%d\n",threedotproduct);
+    //printf("%d %d %d",threecrossproduct.num1, threecrossproduct.num2, threecrossproduct.num3);
+    printf("Vector after projection is [%d %d]",twoDProjection.num1,twoDProjection.num2);
     return 0;
 }
 
