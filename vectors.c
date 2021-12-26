7a65ad0702e6c545f77a66c7734ab7c9e657b12a
Shane-Kyle Chua <90674209+Kairu1999@users.noreply.github.com>
2021-12-27 00:49:13 +0800

    Update vectors.c
    2D matrix addition

--- vectors.c
+++ vectors.c
@@ -1,29 +1,46 @@
 /*Author: Shane-Kyle Chua / Kairu1999*/
 /*This is purely for interest to see how linear algebra's basics can tie in to coding*/
+
 #include <stdio.h>
 
+/*Struct for a 2D Vector*/
 struct TwoDimVector {
     int num1;
     int num2;
 };
 
+/*Struct for a 3D vector*/
 struct ThreeDimVector{
     int num1;
     int num2;
     int num3;
 };
 
+/*Struct for a 2D matrix)
 struct TwoDimMatrix{
     struct TwoDimVector e1;
     struct TwoDimVector e2;
 };
 
+/*Struct for a 3D matrix*/
 struct ThreeDimMatrix{
     struct ThreeDimVector e1;
     struct ThreeDimVector e2;
     struct ThreeDimVector e3;
 };
 
+//Two dimensional Matrix Addition
+struct TwoDimMatrix matrixAdd(struct TwoDimMatrix m1, struct TwoDimMatrix m2){
+    
+    struct TwoDimMatrix NewMatrix;
+    
+    NewMatrix.e1.num1 =  m1.e1.num1 + m2.e1.num1;
+    NewMatrix.e1.num2 =  m1.e1.num2 + m2.e1.num2;
+    NewMatrix.e2.num1 =  m1.e2.num1 + m2.e2.num1;
+    NewMatrix.e2.num2 =  m1.e2.num2 + m2.e2.num2;
+    
+    return NewMatrix;
+}
 /*Calculate the 2 dimensional dot product*/
 int TwoDimDotProduct(struct TwoDimVector vector1, struct TwoDimVector vector2){
     int dotproduct = (vector1.num1 * vector2.num1) + (vector1.num2 * vector2.num2);
@@ -67,11 +84,25 @@ struct ThreeDimVector crossProduct(struct ThreeDimVector vector1, struct ThreeDi
     
     return vector3;
 }
+
+/*Purpose of main is to test stuff*/
 int main()
 {
+    
     int x1 = 1, y1 = 2, x2 = 3,y2 = 4, z1 = 5, z2 = 6;
-    struct TwoDimVector v1, v2;
-    struct ThreeDimVector v3,v4;
+    // struct TwoDimVector v1, v2;
+    // struct ThreeDimVector v3,v4;
+    struct TwoDimMatrix m1, m2;
+    m1.e1.num1 = x1;
+    m1.e1.num2 = y1;
+    m1.e2.num1 = x2;
+    m1.e2.num2 = y2;
+    
+    //make both the same for ease
+    m2.e1.num1 = x1;
+    m2.e1.num2 = y1;
+    m2.e2.num1 = x2;
+    m2.e2.num2 = y2;
     v1.num1 = x1;
     v1.num2 = y1;
     v2.num1 = x2;
@@ -86,10 +117,17 @@ int main()
     int threedotproduct = ThreeDimDotProduct(v3,v4);
     struct ThreeDimVector threecrossproduct = crossProduct(v3,v4);
     struct TwoDimVector twoDProjection = projection(v1,v2);
+    struct TwoDimMatrix addMatrix = matrixAdd(m1,m2);
     //printf("%d\n",dotproduct);
     //printf("%d\n",threedotproduct);
     //printf("%d %d %d",threecrossproduct.num1, threecrossproduct.num2, threecrossproduct.num3);
-    printf("Vector after projection is [%d %d]",twoDProjection.num1,twoDProjection.num2);
+    // printf("Vector after projection is [%d %d]",twoDProjection.num1,twoDProjection.num2);
+    printf("New Matrix after addition: [%d %d | %d %d]",addMatrix.e1.num1,addMatrix.e1.num2, 
+            addMatrix.e2.num1, addMatrix.e2.num2);
+            
+    /*Exit*/
     return 0;
 }
+
+
 
